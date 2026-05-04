b4842337d31404da69d3daa6e2f65a8cc63c0303
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2026-05-04 14:22:28 +0200

    Fixed, ready to go

--- config.py
+++ config.py
@@ -5,10 +5,14 @@
 
 import os
 import os.path
+import sys
 
 
 #Directories
-PROJECTROOT = open(.cw, r).read().rstrip()
+try:
+    PROJECTROOT = open(".cw", "r").read().rstrip()
+except FileNotFoundError:
+    PROJECTROOT = os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
 DATA = os.path.join(PROJECTROOT, "Data")
 FIGURES = os.path.join(PROJECTROOT, "Figures")
 
@@ -16,3 +20,4 @@ formats=['png', 'pdf', 'svg']
 
 #Miscellaneous
 SUPPRESS_INFORMATIVE_PRINT = False
+print(PROJECTROOT)
